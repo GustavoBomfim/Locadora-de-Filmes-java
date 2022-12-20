7bc7aa50b2f5b29362cb368a89ac83edf1aa8627
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-12-19 23:44:20 -0300

    Implementando a inserção de filmes

--- application/Program.java
+++ application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws SQLException {
@@ -42,6 +43,41 @@ public class Program {
         }
 
          */
+        inserir(connection);
+
+        //Receber inputs do usuário
+        /*Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cadastre o nome do filme: ");
+        String nome = sc.next();
+
+        System.out.print("Cadastre o genero do filme: ");
+        String genero = sc.next();
+
+        System.out.print("Cadastre o ator principal do filme: ");
+        String ator_principal = sc.next();
+
+        System.out.print("Cadastre o diretor do filme: ");
+        String diretor = sc.next();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO FILME (nome, genero, ator_principal, diretor) VALUES (?,?,?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setString(1, nome);
+        preparedStatement.setString(2, genero);
+        preparedStatement.setString(3, ator_principal);
+        preparedStatement.setString(4, diretor);
+
+        preparedStatement.execute();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        while(resultSet.next()){
+            Integer id = resultSet.getInt(1);
+            System.out.println("O id criado foi " + id);
+        }
+
+         */
+
+
 
 
         connection.close();
@@ -74,6 +110,41 @@ public class Program {
             System.out.println("---------------------------------------------------------------------------");
 
         }
+    }
+
+    public static void inserir(Connection connection) throws SQLException{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cadastre o nome do filme: ");
+        String nome = sc.next();
+
+        sc.next();
+        System.out.print("Cadastre o genero do filme: ");
+        String genero = sc.next();
+
+        sc.next();
+        System.out.print("Cadastre o ator principal do filme: ");
+        String ator_principal = sc.next();
+
+        sc.next();
+        System.out.print("Cadastre o diretor do filme: ");
+        String diretor = sc.next();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO FILME (nome, genero, ator_principal, diretor) VALUES (?,?,?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setString(1, nome);
+        preparedStatement.setString(2, genero);
+        preparedStatement.setString(3, ator_principal);
+        preparedStatement.setString(4, diretor);
+
+        preparedStatement.execute();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        while(resultSet.next()){
+            Integer id = resultSet.getInt(1);
+            System.out.println("O id criado foi " + id);
+        }
+
     }
 
 
